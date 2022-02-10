9fe43ae6c8fe92d4a96e3de991aa3cba7b55fdf4
Brendan <brendankam1990@gmail.com>
2022-02-10 00:01:16 -0400

    refactor: Changed file to makeit more logical
     - Removed base_manager
     - User Manager to API Manager
     - Moved base_manager code to api_manager

--- snaptrade/managers/api_manager.py
+++ snaptrade/managers/api_manager.py
@@ -4,21 +4,13 @@ from snaptrade.models import User
 
 class UserManager(BaseManager):
     def __init__(self, client_id, consumer_key, user_id=None, user_secret=None):
-        super().__init__(client_id, consumer_key, return_response_as_dict=False)
-        self.user_id = user_id
-        self.user_secret = user_secret
+        super().__init__(client_id, consumer_key, user_id, user_secret, return_response_as_dict=False)
 
     def register_user(self):
         user_details = self.api_client.register_user(self.user_id)
 
-        user = User.init_from_user_details(user_details, namespace=True)
-
-        self.user_secret = user.user_secret
-
-        return user
-
-    def generate_new_user_secret(self):
-        user_details = self.api_client.generate_new_user_secret(self.user_id)
+        if user_details.class_name == "ErrorMessage":
+            return user_details
 
         user = User.init_from_user_details(user_details, namespace=True)
 
@@ -27,11 +19,17 @@ class UserManager(BaseManager):
         return user
 
     def delete_user(self):
-        user_delete_details = self.api_client.delete_user(self.user_id, self.user_secret)
+        user_delete_details = self.api_client.delete_user(self.user_id)
 
-        return True
+        if user_delete_details.class_name == "ErrorMessage":
+            return user_delete_details
+
+        return user_delete_details
 
     def get_user_login_redirect_uri(self):
         redirect_uri = self.api_client.get_user_login_redirect_uri(self.user_id, self.user_secret)
+
+        if redirect_uri.class_name == "ErrorMessage":
+            return redirect_uri
 
         return redirect_uri.redirectURI
