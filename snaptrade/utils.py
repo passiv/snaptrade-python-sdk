0d4748f8fb58e59930651a645e07cbe956c49573
Brendan <brendankam1990@gmail.com>
2022-02-10 23:32:04 -0400

    feat(no): Added get_utc-time method

--- snaptrade/utils.py
+++ snaptrade/utils.py
@@ -1,24 +1,33 @@
 from types import SimpleNamespace
 import time
+from datetime import datetime, timezone
 import json
 
-class SnapTradeUtils:
 
+class SnapTradeUtils:
     @classmethod
     def convert_to_simple_namespace(cls, data):
         """Converts json data into python object"""
-        if type(data) == dict:
-            serialized_data = json.dumps(data)
+        if type(data) == list:
+            data = {"class_name": "SimpleNamepace", "data": data}
         else:
-            serialized_data = data
+            data["class_name"] = "SimpleNamepace"
+        serialized_data = json.dumps(data)
 
         return json.loads(serialized_data, object_hook=lambda d: SimpleNamespace(**d))
 
     @classmethod
     def get_api_endpoints(cls):
-        with open('./snaptrade/endpoints.json', 'r') as f:
+        with open("./snaptrade/api_client/endpoints.json", "r") as f:
             return json.load(f)
 
     @classmethod
     def get_epoch_time(cls):
         return int(time.time())
+
+    @classmethod
+    def get_utc_time(cls, string_format=None):
+        if string_format:
+            return datetime.now(timezone.utc).strftime(string_format)
+        else:
+            return datetime.now(timezone.utc)
