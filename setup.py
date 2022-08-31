daa9b5421e4a9eab7f34338c6de8c11ddb1e2489
Brendan <brendankam1990@gmail.com>
2022-08-31 17:02:05 -0300

    chore: Sync repo with code private repo
    All code changes should be backwards compatible

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.MD", "r") as f:
 
 setup(
     name="snaptrade",
-    version="1.0.3",
+    version="1.0.4",
     description="A Python implementation of SnapTrade API client library",
     packages=find_packages(),
     package_data={"snaptrade": ["api_client/endpoints.json"]},
@@ -23,5 +23,6 @@ setup(
     ],
     install_requires=[
         "requests",
+        "pycryptodome",
     ],
 )
